ffac99c03bc8bb5a011edf2d2fa41dc703e0cfa9
alex <alexanderszzzzzz@gmail.com>
2025-07-12 15:02:25 +0300

    add security measures: - add Rate Limiting & Throttling; - enable CORS; - Remove Sensitive Headers (X-Powered-By, ETag, Server version); - (Provided by Prisma ORM) Input Validation and Protection of SQL Injections;

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,36 @@ import { AppService } from './app.service';
 import { DatabaseModule } from '@src/database/database.module';
 import { MyLoggerModule } from '@src/my-logger/my-logger.module';
 import { CustomersModule } from '@src/customers/customers.module';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [DatabaseModule, MyLoggerModule, CustomersModule],
+  imports: [
+    DatabaseModule,
+    MyLoggerModule,
+    CustomersModule,
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 6000,
+        limit: 3,
+        blockDuration: 1000,
+      },
+      {
+        name: 'long',
+        ttl: 60000,
+        limit: 100,
+        blockDuration: 5000,
+      },
+    ]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,21 @@ import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from '@common/filters/all-exceptions.filter';
 import { TransformInterceptor } from '@common/interceptors/transform.interceptor';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.disable('x-powered-by');
+  app.disable('etag');
 
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
   app.useGlobalInterceptors(new TransformInterceptor());
   app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: process.env.ALLOWED_ORIGINS?.split(','),
+  });
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
